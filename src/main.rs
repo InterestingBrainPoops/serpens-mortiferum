859494adf0055830834721ae3a7cd0a05a4de51f
InterestingBrainPoops <satasiyahet@gmail.com>
2021-04-23 14:21:08 -0700

    did way too much stuff for one commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use serde::Deserialize;
 use serde_json::json;
 use warp::http::StatusCode;
 use warp::Filter;
 use warp::Rejection;
-/// Given a board state, return the best move along w/ the confidence. (Basically the fitness of the move)
-fn gen_move<'a>(possible_moves: &'a [&'static str]) -> (&'a str, f32) {
-    return (&possible_moves[0], 0.0);
-}
+use movegen::*;
+pub mod movegen;
+
 #[tokio::main]
 async fn main() {
     let index = warp::path::end().map(|| {
@@ -26,10 +24,10 @@ async fn main() {
     let get_move = warp::path("move")
         .and(warp::post())
         .and(warp::body::json())
-        .and_then(|sent_move: Move| async move {
+        .and_then(|sent_move: engine::Move| async move {
             // move logic
             let possible_moves = ["up", "down", "left", "right"];
-            let out_move = gen_move(&possible_moves);
+            let out_move = search::gen_move(&sent_move);
             println!(
                 "Turn : {}, Moving: {}, with confidence {}",
                 3, out_move.0, out_move.1
@@ -48,78 +46,4 @@ async fn main() {
         .expect("PORT is not a valid port number");
     println!("Listening on port {}", port);
     warp::serve(routes).run(([0, 0, 0, 0], port)).await;
-}
-
-/// /move request sent by the battlesnake engine.
-#[derive(Debug, Deserialize)]
-struct Move {
-    game: SentGame,
-    turn: u32,
-    board: Board,
-    you: Battlesnake,
-}
-
-/// Game ID and more info, such as the timeout.
-#[derive(Debug, Deserialize)]
-struct SentGame {
-    id: String,
-    timeout: u128,
-}
-
-/// Board that is sent by the engine in the request.
-#[derive(Debug, Deserialize)]
-struct Board {
-    height: u8,
-    width: u8,
-    food: Vec<Pos>,
-    hazards: Vec<Pos>,
-    snakes: Vec<Battlesnake>,
-}
-
-/// Position, makes it much easier to work w/.
-#[derive(Debug, Deserialize, Eq, PartialEq)]
-struct Pos {
-    x: u8,
-    y: u8,
-}
-
-/// the battlesnake struct, sent by the engine.
-#[derive(Debug, Deserialize, Eq, PartialEq)]
-struct Battlesnake {
-    id: String,
-    name: String,
-    health: u8,
-    body: Vec<Pos>,
-    latency: i16,
-    head: Pos,
-    length: u16,
-    shout: String,
-}
-
-/// Cast into from the board state.
-/// This one is actually used.
-/// Contains a list of all snakes, and the size of the board. This assumes that the board is a square in shape.
-#[derive(Debug)]
-struct SmallBoard {
-    snakes: Vec<SmallSnake>,
-    size: u8,
-}
-
-/// Small storage space snake.
-/// Used in the search.
-#[derive(Debug)]
-struct SmallSnake {
-    id: String,
-    health: u8,
-    body: Vec<Pos>,
-    head: Pos,
-}
-
-/// Small Move,
-/// Holds the direction (UDLR)
-/// and the ID.
-#[derive(Debug)]
-struct SmallMove {
-    id: String,
-    dir: String,
 }
---
+++ src/movegen/search.rs
@@ -0,0 +1,5 @@
+/// Given a board state, return the best move along w/ the confidence. (Basically the fitness of the move)
+pub fn gen_move(state : &super::engine::Move) -> (&'static str, f32) {
+    
+    return (&"up", 0.0);
+}
